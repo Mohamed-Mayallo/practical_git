db71e97a5cd404a1b6f049b636a39e540d38aabb
Mohamed-Mayallo <reckoning58@gmail.com>
2019-06-12 23:44:01 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * git version
+ *
+ * git init
+ *
+ * git config -l
+ * git config --list
+ *
+ * git config --global user.name "Mohamed-Mayallo"
+ * git config --global user.email "reckoning58@gmail.com"
+ *
+ * git remote add origin https://github.com/Mohamed-Mayallo/practical_git.git
+ * git remote -v
+ *
+ * git status
+ *
+ * git add .
+ * git add views/*
+ *
+ * git commit -m "First commit"
+ * git commit -am "Commit and add"
+ *
+ * git push origin -u master
+ */
